c3511da20d5c17b293f809f1139c3558a91ede70
Venancio Augusto <41402809+venanciokranvoski@users.noreply.github.com>
2023-02-15 01:10:59 -0300

    useEffect

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
-import { Button, SafeAreaView } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Alert, Button, SafeAreaView } from 'react-native';
 import Header from "./src/components/Header";
 
-const App : React.FC = () => {
-    const [test, setTest] = useState('venancio')
 
+
+const App : React.FC = () => {
+    const [test, setTest] = useState<string>('venancio')
+    const [t, setT] = useState<string>('Ola')
+
+
+    // 1 - initialization
+    // 2 - Mounting
+    // 3 - Updation
+    // 4 - Unmounting
+    useEffect(()=> {
+    console.log('montou');
+     if(test === 'venancio'){
+       setT('gatoooo')
+     }else {
+        setT('gatinhaa')
+     }
+     return ()=> Alert.alert('component foi desmontado')
+    }, [test, t])
+
+    /**
+     * callback
+     *
+     */
     const handlePressButton = () => {
         setTest(old => {
             if(old === 'venancio'){
@@ -17,7 +39,7 @@ const App : React.FC = () => {
 
     return(
     <SafeAreaView>
-        <Header title = "Sr" name={test}  />
+        <Header title={t} name={test}  />
         <Button title="click" onPress={handlePressButton} />
     </SafeAreaView>
 
