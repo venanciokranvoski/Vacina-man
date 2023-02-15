5076d9c6fa54f138d7ba7a69e33ff53460be540b
Venancio Augusto <41402809+venanciokranvoski@users.noreply.github.com>
2023-02-15 02:17:17 -0300

    States and Uses

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Alert, Button, SafeAreaView } from 'react-native';
+import React, { useState, useEffect, useCallback, useMemo, useRef } from "react";
+import { Alert, Button, SafeAreaView, StyleSheet, TextInput } from 'react-native';
 import Header from "./src/components/Header";
 
 
@@ -13,37 +13,72 @@ const App : React.FC = () => {
     // 2 - Mounting
     // 3 - Updation
     // 4 - Unmounting
-    useEffect(()=> {
-    console.log('montou');
-     if(test === 'venancio'){
-       setT('gatoooo')
-     }else {
-        setT('gatinhaa')
-     }
-     return ()=> Alert.alert('component foi desmontado')
-    }, [test, t])
+    // useEffect(()=> {
+    //  if(test === 'venancio'){
+    //    setT('gatoooo')
+    //  }else {
+    //     setT('gatinhaa')
+    //  }
+    // //  return ()=> Alert.alert('component foi desmontado')
+    // }, [test, t])
 
     /**
      * callback
      *
      */
-    const handlePressButton = () => {
+
+
+
+    /**
+     * useCallback mantem uma funçãoo
+     *
+     */
+    console.log(`state do começoo ${test}`);
+
+    const handlePressButton = useCallback(()=> {
         setTest(old => {
             if(old === 'venancio'){
+                console.log(`function  ${test}`);
                 return 'juju';
             }else {
+                console.log(`senaoi ${test}`);
+
                 return 'venancio';
             }
         })
-    }
+    }, [])
+
+    /**
+     * useMemo mantem um value
+     */
+    //   const nameChanged = useMemo(()=> {
+    //      return `${test} | ${new Date()}`
+    //   }, [test])
+
+      /**
+       * Refs
+       */
+      const inputRef = useRef<TextInput>({} as TextInput);
 
     return(
     <SafeAreaView>
-        <Header title={t} name={test}  />
-        <Button title="click" onPress={handlePressButton} />
+        <Header title={t} name='teste'  />
+        <Button title="click" onPress={()=> setTest('tuijidjijds')}/>
+
+        <TextInput ref={inputRef}  style={s.in} onChangeText={txt => setTest(txt)} />
     </SafeAreaView>
 
     )
+
+
     }
 
 export default App;
+
+const s = StyleSheet.create({
+    in: {
+        margin: 10,
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        padding: 15
+    }
+})
