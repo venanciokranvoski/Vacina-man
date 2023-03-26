5ebe348feb4e2a870cadc1bae11ba0ce7b3a2edf
Venancio Augusto <41402809+venanciokranvoski@users.noreply.github.com>
2023-03-26 01:36:49 -0300

    feat-SetParams

--- App.tsx
+++ App.tsx
@@ -1,64 +1,16 @@
-/* eslint-disable react/jsx-no-duplicate-props */
-/* eslint-disable react-native/no-inline-styles */
-import React, {useRef} from 'react';
+import React from 'react';
 import {ThemeProvider} from 'styled-components';
-import {Alert, View} from 'react-native';
-import Header from '~/components/Header';
 import useApperance from '~/hooks/useApperance';
-import TextMain from '~/components/Text';
-import Icon from '~/components/Icon';
-import Button from '~/components/Button';
-import Separator from '~/components/Separator';
-import Input from '~/components/Input';
-import {inputValueRef} from '~/components/Input/types';
 import {NavigationContainer} from '@react-navigation/native';
+import Routes from '~/navigation';
 
 const App: React.FC = () => {
   const {theme} = useApperance();
 
-  const refEmail = useRef<inputValueRef>({value: ''});
-  const refPassword = useRef<inputValueRef>({value: ''});
-
-  const handlePress = () => {
-    Alert.alert(
-      refEmail.current?.value || 'nao tem nada',
-      refPassword.current?.value || 'sem nenhuma senha ',
-    );
-  };
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <View>
-          <Header />
-          <TextMain typography="h4" color="primary">
-            venancio
-          </TextMain>
-          <View style={{flexDirection: 'row', alignItems: 'center'}}>
-            <Icon icon="listening" size={130} />
-            <Separator width={160} />
-            <Icon icon="listening" size={130} />
-          </View>
-        </View>
-
-        <View>
-          <Input
-            ref={refEmail}
-            icon="listening"
-            iconPosition="left"
-            label="E-mail"
-          />
-          <Input
-            ref={refPassword}
-            label="User not exist"
-            iconPosition="left"
-            secureTextEntry
-            label="Senha"
-          />
-        </View>
-
-        <Button mode="contained" onPress={handlePress}>
-          Change settings
-        </Button>
+        <Routes />
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/screen/Profile/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {Container} from './styled';
+
+const Profile: React.FC = () => {
+  return (
+    <Container>
+      <Text>Profile</Text>
+    </Container>
+  );
+};
+export default Profile;
